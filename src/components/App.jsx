9bebbde49f65b1e6ec4f058d712e748d44d6fd90
Willy Corzo <wjcorzol@gmail.com>
2021-12-09 23:38:25 -0500

    .

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+	return (
+		<h1>Hola a todos sean Bienvenidos somos el EQUIPO 6</h1>
+	);
+}
+
+export default App;
